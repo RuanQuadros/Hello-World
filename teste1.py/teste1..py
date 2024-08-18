e7712ad9535b1ba8301de3cde8d551e4c7135c98
Ruan <ruanq54@gmail.com>
2024-08-18 01:46:14 -0300

    Teste

--- teste1.py/teste1..py
+++ teste1.py/teste1..py
@@ -1,1 +1,20 @@
 print ('Hello, World !')
+
+# Sistema para saber porcentagem de imposto de renda em Python
+
+salario_base = 2259.20
+try:
+    salario = float(input('Digite seu salario: '))
+
+    if salario <= salario_base:
+        print('Isento de imposto')
+    elif salario <= 2826.65:
+        print('Imposto de 7,5% ')
+    elif salario <= 3751.05:
+        print('Imposto de 15% ')
+    elif salario <= 4664.68:
+        print('Imposto de 22,5% ')
+    elif salario >= 4664.68:
+        print('Imposto de 27,5% ')
+except ValueError:
+    print('Você não digitou em numeros.')
